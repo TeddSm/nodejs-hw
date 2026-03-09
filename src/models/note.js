21dad6800c0a34551b2ab48d8b7ce230da936a53
Mykhailo Drogobetskyi <C:\Users\DrogobetskyiM\AppData\Roaming\The Bat!>
2026-03-09 17:12:30 +0200

    complete tasks

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import { TAGS } from '../contacts/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -14,7 +15,7 @@ const noteSchema = new Schema(
     },
     tag: {
       type: String,
-      enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+      enum: TAGS,
       default: 'Todo',
     },
   },
@@ -22,5 +23,7 @@ const noteSchema = new Schema(
     timestamps: true
   },
 );
+
+noteSchema.index({ title: 'text', content: 'text' });
 
 export const Note = model('Note', noteSchema);
