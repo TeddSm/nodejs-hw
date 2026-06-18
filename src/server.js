a42a27a01f07b7d239ec9e54ce334754112133b6
Mykhailo Drogobetskyi <C:\Users\DrogobetskyiM\AppData\Roaming\The Bat!>
2026-06-18 15:30:43 +0300

    add eslint

--- src/server.js
+++ src/server.js
@@ -24,5 +24,5 @@ app.use(errorHandler);
 await connectMongoDB();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
